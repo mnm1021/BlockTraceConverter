88383b2129a9ac95293cd37dd0dc9f92658051ef
yongseok_jin <mnm102211@gmail.com>
2018-09-06 18:27:26 +0900

    added FIO into current git repo

---
+++ fio/steadystate.h
@@ -0,0 +1,59 @@
+#ifndef FIO_STEADYSTATE_H
+#define FIO_STEADYSTATE_H
+
+#include "thread_options.h"
+
+extern void steadystate_check(void);
+extern void steadystate_setup(void);
+extern void td_steadystate_init(struct thread_data *);
+
+extern bool steadystate_enabled;
+
+/*
+ * For steady state detection
+ */
+struct steadystate_data {
+	double limit;
+	unsigned long long dur;
+	unsigned long long ramp_time;
+	bool pct;
+
+	unsigned int state;
+
+	unsigned int head;
+	unsigned int tail;
+	unsigned long *iops_data;
+	unsigned long *bw_data;
+
+	double slope;
+	double criterion;
+	double deviation;
+
+	unsigned long long sum_y;
+	unsigned long long sum_x;
+	unsigned long long sum_x_sq;
+	unsigned long long sum_xy;
+	unsigned long long oldest_y;
+
+	struct timeval prev_time;
+	unsigned long long prev_iops;
+	unsigned long long prev_bytes;
+};
+
+enum {
+	__FIO_SS_IOPS		= 1,
+	__FIO_SS_BW		= 2,
+	__FIO_SS_SLOPE		= 4,
+	__FIO_SS_ATTAINED	= 8,
+	__FIO_SS_RAMP_OVER	= 16,
+	__FIO_SS_LAST		= 32,
+
+	FIO_SS_IOPS		= __FIO_SS_IOPS,
+	FIO_SS_IOPS_SLOPE	= __FIO_SS_IOPS | __FIO_SS_SLOPE,
+	FIO_SS_BW		= __FIO_SS_BW,
+	FIO_SS_BW_SLOPE		= __FIO_SS_BW | __FIO_SS_SLOPE,
+};
+
+#define STEADYSTATE_MSEC	1000
+
+#endif
